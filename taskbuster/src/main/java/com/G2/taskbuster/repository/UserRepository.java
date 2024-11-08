c9a5b9fade08739669edea70b24914d2114980f9
superdaks <raprapsaceda11@gmail.com>
2024-11-09 01:04:26 +0800

    update

--- taskbuster/src/main/java/com/G2/taskbuster/repository/UserRepository.java
+++ taskbuster/src/main/java/com/G2/taskbuster/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.G2.taskbuster.repository;
 
 import com.G2.taskbuster.entity.UserEntity;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
