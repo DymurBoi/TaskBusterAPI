1b7826d79e285e8a555207fc20cc9ccb4c14eedd
DymurBoi <dymurboi@gmail.com>
2024-11-10 13:25:26 +0800

    dilit

--- taskbuster/taskbuster/src/main/java/com/G2/taskbuster/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.G2.taskbuster.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
